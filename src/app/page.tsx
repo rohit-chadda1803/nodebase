992e594e42e94901b4b1dfb8b4ed4ac1b9663bc1
Rohit Chadda <rohitchadda129@gmail.com>
2026-03-25 17:18:42 +0530

    06-background-jobs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-// "use client" 
+"use client" 
 
 import { authClient } from "@/lib/auth-client";
 import { Button } from "@/components/ui/button";
@@ -8,19 +8,36 @@ import { requireAuth } from "@/lib/auth-utils";
 import { caller } from "@/trpc/server";
 import { LogoutButton } from "./logout";
 
-const Page =  async() => {
-   await requireAuth();
+import { QueryClient, useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { useTRPC } from "@/trpc/client";
+import { toast } from "sonner";
+
+
+const Page =  () => {
+  //await requireAuth();
   //const { data } = authClient.useSession() ;
 
-  const data = await caller.getUsers() ; 
+  // const data = await caller.getUsers() ; 
+  const trpc = useTRPC() ; 
+  const queryClient = useQueryClient();
+  const {data} = useQuery(trpc.getWorkflows.queryOptions()) ;
+  const create = useMutation(trpc.createWorkflow.mutationOptions({
+    onSuccess : ()=>{
+      toast.success("job queued") ;
+    }
+  })) ;
 
   return (
     //<div className={cn("text-red-500 font-extrabold" , something && "text-green-500")}>text</div>
-    <div className = "min-h-screen min-w-screen flex items-center justify-center">
+    <div className = "min-h-screen min-w-screen flex items-center justify-center flex-col gap-y-6">
       Protected servr comp 
       <div>
         {JSON.stringify(data ,null ,2)}
       </div>
+
+      <Button disabled={create.isPending} onClick={()=> create.mutate()}>
+        Create Workflow
+      </Button>
       <LogoutButton/>
     </div>
   );
---
+++ src/inngest/functions.ts
@@ -0,0 +1,23 @@
+import { inngest } from "./client";
+import prisma from "@/lib/db";
+export const helloWorld = inngest.createFunction(
+  { id: "hello-world"   },
+  { event: "test/hello.world" },
+  async ({ event, step }) => {
+
+    // fetching the video  . 
+    await step.sleep("fetching", "5s" ) ; 
+    // transcribing . 
+    await step.sleep("transcribing", "5s");
+    // sending transcription to Ai. 
+    await step.sleep("sending-to-ai", "5s");
+   
+    await step.run("create-workflow" , () => {
+         return prisma.workflow.create({
+            data :{
+                name : "workflow-from-inngest" , 
+            }
+         })
+    }) ; 
+  }
+);
